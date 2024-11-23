a4bded913de3c13788818926437226e04590e58a
rodrigoalmeida2 <rodrigoalmeida350.ra@gmail.com>
2024-11-22 22:35:56 -0300

    junção

--- juncao.py
+++ juncao.py
@@ -1,22 +1,26 @@
 import whisper
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
+import yt_dlp
 
+# Função para baixar o áudio
+def download_audio(url, output_filename="audio"):
+    ydl_opts = {
+        'format': 'bestaudio/best',  # Escolhe o melhor formato de áudio disponível
+        'outtmpl': output_filename,  # Nome do arquivo de saída
+        'postprocessors': [{  # Conversão do áudio para formato específico (se necessário)
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',  # Define o formato do áudio (mp3 neste caso)
+            'preferredquality': '192',  # Define a qualidade do áudio (opcional)
+        }]
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
 
 def transcricao():
     modelo = whisper.load_model("small")
     result = modelo.transcribe("audio.mp3")
     return result["text"]
-
-
-def PT_to_EN(text):
-    tokenizer = AutoTokenizer.from_pretrained("unicamp-dl/translation-pt-en-t5")
-
-    model = AutoModelForSeq2SeqLM.from_pretrained("unicamp-dl/translation-pt-en-t5")
-
-    pten_pipeline = pipeline('text2text-generation', model=model, tokenizer=tokenizer)
-
-    print(pten_pipeline(f"translate Portuguese to English: {text}"))
-
 
 def Summa(pt_to_en):
     summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
@@ -25,15 +29,15 @@ def Summa(pt_to_en):
 
     print(summarizer(ARTICLE, max_length=200, min_length=50, do_sample=False))
 
-
-def EN_to_PT(resumo):
-    pipe = pipeline("translation", model="Helsinki-NLP/opus-mt-tc-big-en-pt")
-    print(pipe(f">>por<< {resumo}"))
-
-
 def main():
     trans = transcricao()
-    print(trans)
     #PT_to_EN(trans)
+    Summa(trans)
+    # URL do vídeo do qual você quer baixar o áudio
+    video_url = "https://youtu.be/GVTBZfeTDfU"
 
+    # Baixar o áudio
+    download_audio(video_url)
+
+    print(f"Download concluído. O áudio foi salvo como 'audio.mp3'")
 main()
