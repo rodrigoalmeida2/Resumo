b7d5fa60d9581b4959fb6eaf543a05923b656ad5
rodrigoalmeida2 <rodrigoalmeida350.ra@gmail.com>
2024-12-02 12:25:38 -0300

    divide texto

---
+++ Divide_Texto.py
@@ -0,0 +1,21 @@
+class TextSplitter:
+    """Classe responsável por dividir um texto em partes menores."""
+    
+    @staticmethod
+    def split(text, max_tokens):
+        """Divide o texto em partes menores com base no limite de tokens."""
+        words = text.split()
+        parts = []
+        current_part = []
+        current_length = 0
+
+        for word in words:
+            current_length += len(word) + 1  # Inclui o espaço
+            if current_length > max_tokens:
+                parts.append(' '.join(current_part))
+                current_part = [word]
+                current_length = len(word) + 1
+            else:
+                current_part.append(word)
+        parts.append(' '.join(current_part))
+        return parts
