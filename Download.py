d9f91c52110fcd936818c9bc363651c88f010633
rodrigoalmeida2 <rodrigoalmeida350.ra@gmail.com>
2024-12-02 12:24:26 -0300

    cg

---
+++ Download.py
@@ -0,0 +1,21 @@
+import yt_dlp
+
+class AudioDownloader:
+    """Classe responsável por baixar o áudio de um vídeo do YouTube."""
+    
+    @staticmethod
+    def download(video_url, output_filename="audio.mp3"):
+        """Baixa o áudio de um vídeo do YouTube."""
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'outtmpl': output_filename,
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }]
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([video_url])
+        print(f"Áudio baixado como {output_filename}")
+        return output_filename
