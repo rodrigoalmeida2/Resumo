20f0e07a1d7a42c78ed4889104fc3aae46b6cd40
Rodrigo Almeida <rodrigoalmeida350.ra@gmail.com>
2024-12-02 13:26:18 -0300

    Delete teste directory

--- teste/Translation.py
+++
@@ -1,5 +0,0 @@
-from transformers import pipeline
-pipe = pipeline("translation", model="Helsinki-NLP/opus-mt-tc-big-en-pt")
-print(pipe(">>por<< Tom tried to stab me."))
-
-# expected output: O Tom tentou esfaquear-me.
--- teste/transcricao.py
+++
@@ -1,4 +0,0 @@
-import whisper
-modelo = whisper.load_model("small")
-result = modelo.transcribe("audio.mp3")
-print(result["text"])
