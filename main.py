3347baa53cb781b02115ab70207c6456bb8a4db2
rodrigoalmeida2 <rodrigoalmeida350.ra@gmail.com>
2024-12-05 13:41:28 -0300

    state

--- main.py
+++ main.py
@@ -17,60 +17,60 @@ st.write("Insira a URL de um vídeo do YouTube para transcrever e gerar resumos.
 # Entrada para URL do vídeo
 video_url = st.text_input("URL do vídeo:", "")
 
-def botao():
-    # Botão para processar o vídeo
-    if st.button("Processar"):
-        if video_url.strip():
-            # Token da API Hugging Face
-            token = os.getenv('ACCESS_TOKEN')
-            
-            # Inicializa o summarizer
-            summarizer = VideoSummarizer(token)
-            
-            with st.spinner("Processando o vídeo, por favor aguarde..."):
-                try:
-                    # Processa o vídeo
-                    summaries = summarizer.process_video(video_url)
-                    
-                    # Mostra os resumos na interface
-                    st.success("Resumo gerado com sucesso!")
-                    st.write("### Resumos:")
-                    for idx, summary in enumerate(summaries, start=1):
-                        st.write(f"{summary}")
-                    
-                    # Salva os resumos em arquivos
-                    pdf_file = "resumo_video.pdf"
-                    word_file = "resumo_video.docx"
-                    SaveSummaries.save_to_pdf(summaries, pdf_file)
-                    SaveSummaries.save_to_word(summaries, word_file)
 
-                    
-                except Exception as e:
-                    st.error(f"Ocorreu um erro: {str(e)}")
-        else:
-            st.warning("Por favor, insira uma URL válida.")
+# Botão para processar o vídeo
+if st.button("Processar"):
+    if video_url.strip():
+        # Token da API Hugging Face
+        token = os.getenv('ACCESS_TOKEN')
+        
+        # Inicializa o summarizer
+        summarizer = VideoSummarizer(token)
+        
+        with st.spinner("Processando o vídeo, por favor aguarde..."):
+            try:
+                # Processa o vídeo
+                summaries = summarizer.process_video(video_url)
+                
+                # Mostra os resumos na interface
+                st.success("Resumo gerado com sucesso!")
+                st.write("### Resumos:")
+                for idx, summary in enumerate(summaries, start=1):
+                    st.write(f"{summary}")
+                
+                # Salva os resumos em arquivos
+                pdf_file = "resumo_video.pdf"
+                word_file = "resumo_video.docx"
+                SaveSummaries.save_to_pdf(summaries, pdf_file)
+                SaveSummaries.save_to_word(summaries, word_file)
 
-
-def seg(pdf_file, word_file):
-    # Botões para baixar os arquivos
-    with open(pdf_file, "rb") as pdf:
-        st.download_button(
-            label="Baixar PDF",
-            data=pdf,
-            file_name=pdf_file,
-            mime="application/pdf"
-        )
-
-    with open(word_file, "rb") as docx:
-        st.download_button(
-            label="Baixar Word",
-            data=docx,
-            file_name=word_file,
-            mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-        )
-    
+                # Botões para baixar os arquivos
+                with open(pdf_file, "rb") as pdf:
+                    st.download_button(
+                        label="Baixar PDF",
+                        data=pdf,
+                        file_name=pdf_file,
+                        mime="application/pdf"
+                    )
+                
+                with open(word_file, "rb") as docx:
+                    st.download_button(
+                    label="Baixar Word",
+                    data=docx,
+                    file_name=word_file,
+                    mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+                )
+                
+            except Exception as e:
+                st.error(f"Ocorreu um erro: {str(e)}")
+    else:
+        st.warning("Por favor, insira uma URL válida.")
     os.remove("resumo_video.docx")
     os.remove("resumo_video.pdf")
 
-botao()
-seg("resumo_video.docx", "resumo_video.pdf")
+
+
+
+#botao()
+#seg("resumo_video.pdf")
+#ter("resumo_video.docx") 
